be44a74802e352fbaa56497ef2c757ef067591f0
Guy Cole <guycole@gmail.com>
2020-08-14 13:59:12 -0700

    tweak path, remove obsolete options

--- pytest.sh
+++ pytest.sh
@@ -2,8 +2,8 @@
 #
 # Title: pytest.sh
 # Description: invoke pytest
-# Development Environment: OS X 10.11.6
-# Author: G.S. Cole (guy at shastrax dot com)
+# Development Environment:Ubuntu 18
+# Author:Guy Cole (guycole at gmail dot com)
 #
 #PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 #
@@ -13,13 +13,4 @@ export PYTHONPATH=$HOME/IdeaProjects/mellow-ferret/src
 source ./src/venv/bin/activate
 #
 pytest tests/*.py
-#
-#if [[ $1 = "apigw" ]];
-#then
-#    echo "integration test"
-#    pytest tests/integration_v01/*.py;
-#else
-#    echo "unit test"
-#    pytest tests/unit_v01/*.py
-#fi
 #
