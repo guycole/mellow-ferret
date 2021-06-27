fb6759d43da563d4039290f27c96f0d8d5f35c5b
Guy Cole <guycole@gmail.com>
2021-06-27 01:15:17 -0700

    signal strength and system information

---
+++ src/command_si.py
@@ -0,0 +1,21 @@
+#
+# Title:command_si.py
+# Description: system information
+# Development Environment:Ubuntu 18/Python 3.6.9
+# Author:Guy Cole (guycole at gmail dot com)
+#
+import logging
+
+from bc780 import Bc780
+
+class CommandSg:
+    def __init__(self):
+        self.logger = logging.getLogger()
+
+    def execute(self, command:str, bc780:Bc780):
+        self.logger.info(f"command:{command}")
+        return "SI\r"
+
+# ;;; Local Variables: ***
+# ;;; mode:python ***
+# ;;; End: ***
