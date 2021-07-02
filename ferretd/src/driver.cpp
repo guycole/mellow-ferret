f2a6ec1e054391fda842ad30475912edcdca88ac
Guy Cole <guycole@gmail.com>
2021-07-02 11:02:13 -0700

    migrate to c++ from python

---
+++ ferretd/src/driver.cpp
@@ -0,0 +1,18 @@
+//
+// Title:driver.cpp
+//
+// Description:
+//
+// Development Environment:
+//   Ubuntu 18.04
+//   gcc version 7.5.0
+//
+// Author:
+//   G.S. Cole (guycole at gmail dot com)
+//
+#include<iostream>
+ 
+int main(int argc, char *argv[]) {
+   std::cout << "ferret driver" << std::endl;
+   return 0;
+}
